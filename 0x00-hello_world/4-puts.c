6ca86d26c2d5bf7dd89df9cf8db10ff2f55be0fb
Osbee205 <osborn_essien@yahoo.com>
2022-06-30 16:57:28 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,24 @@
+#!/bin/bash
+#include <stdio>
+
+
+
+/**
+ *
+ * *main
+ *
+ * *
+ *
+ * *Return: Always 0 (Success)
+ *
+ * */
+
+int main(void)
+
+{
+
+	               puts("\"Programming is like building a multilingual puzzle ");
+
+		                      return (0);
+
+}
