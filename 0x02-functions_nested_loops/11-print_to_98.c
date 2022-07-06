6e8f66d042900fde385c7dba4cc6134b7f85854a
Osbee205 <osborn_essien@yahoo.com>
2022-07-06 02:23:16 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,31 +1,29 @@
+#include "main.h"
 #include <stdio.h>
-#include "main.h" 
 
 /**
-* print_to_98 - prints all natural numbers from n to 98,
-* followed by a new line
-* @n: print from this number
-*/
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: natural number to start at
+ */
 void print_to_98(int n)
 {
-int i, j; 
-if (n <= 98)
+while (n != 98)
 {
-for (i = n; i <= 98; i++)
+if (n < 98)
 {
-if (i != 98)
-printf("%d, ", i);
-else if (i == 98)
-printf("%d\n", i);
+printf("%d", n);
+printf(",");
+printf(" ");
+n++;
 }
-else if (n >= 98)
+else
 {
-for (j = n; j >= 98; j--)
-{
-if (j != 98)
-printf("%d, ", j);
-else if (j == 98)
-printf("%d\n", j);
+printf("%d", n);
+printf(",");
+printf(" ");
+n--;
 }
 }
+printf("98");
+printf("\n");
 }
