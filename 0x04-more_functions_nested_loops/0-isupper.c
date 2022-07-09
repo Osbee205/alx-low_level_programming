e9f4bd8ebd86e56b3beb329a5ee4bcfd4006a8dc
Osbee205 <osborn_essien@yahoo.com>
2022-07-08 23:44:46 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,11 +5,9 @@
  * @c: character to be checked
  * Return: 1 if true, 0 if false
  */
-
 int _isupper(int c)
 {
-        if (c >= 'A' && c <= 'Z')
-        return (1);
-
-        return (0);
+if (c >= 'A' && c <= 'Z')
+return (1);
+return (0);
 }
