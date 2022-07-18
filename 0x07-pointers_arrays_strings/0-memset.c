37d4a71b300d058a91cb168f75b47a2142026f4a
Osbee205 <osborn_essien@yahoo.com>
2022-07-18 09:21:26 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,11 +7,12 @@
  * @n: bytes
  * Return: Always 0 (Success)
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+unsigned int i;
 
-	for (i = 0; i < n; i++)
-		s[i] = b;
-	return (s);
+for (i = 0; i < n; i++)
+s[i] = b;
+return (s);
 }
